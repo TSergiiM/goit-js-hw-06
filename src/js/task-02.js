7b8a580ce5b00427679ea0f74c43b2b93e6f139b
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-04 13:26:13 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,8 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
 ingredients.forEach(elem => {
-  const el = document.createElement('li');
-  el.textContent = elem;
-  el.classList.add('item');
-  document.querySelector('ul').append(el);
+  const elemLi = document.createElement('li');
+  elemLi.textContent = elem;
+  elemLi.classList.add('item');
+  document.querySelector('ul').append(elemLi);
 });
