084fb1cb11249fec0c3005cc3ca9bf4ec20a0511
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-05 22:40:38 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const bgColor = document.querySelector('.color');
+
+const onColorSet = btnChangeColor.addEventListener('click', () => {
+  document.body.style.background = getRandomHexColor();
+  bgColor.textContent = getRandomHexColor();
+});
