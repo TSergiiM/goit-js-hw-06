95e3fdf84d6d4efafdbfeaa68555de019068adb6
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-05 21:32:17 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
 
+const onSubmit = event => {
+  event.preventDefault();
+  const options = {
+    Email: event.currentTarget.elements.email.value.trim(),
+    Password: event.currentTarget.elements.password.value.trim(),
+  };
+  if (!options.Email || !options.Password) return alert('всі поля повинні бути заповнені');
+  console.log(options);
+  form.reset();
+};
+
+form.addEventListener('submit', onSubmit);
